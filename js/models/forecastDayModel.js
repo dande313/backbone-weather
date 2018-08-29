f50143891933280483d90cd6d5ff82866f5f60ee
Allen Anderson <andeal@2020spaces.com>
2018-08-29 14:27:40 -0400

    got rid of bogus urlRoot

--- js/models/forecastDayModel.js
+++ js/models/forecastDayModel.js
@@ -5,7 +5,6 @@ define([
 	'use strict';
 
 	var ForecastDay = Backbone.Model.extend({
-		urlRoot:"http://api.openweathermap.org/data/2.5/forecast?lat=27&lon=-85&units=imperial&appid=e0e31fecb53d51f5aa959eee6bc014f1",
 
 		parse: function(data){
 			var parsedData = {
